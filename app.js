0c92379f3e775c96f6c434ef07859efbfde44f88
Karuna Chaudhary <karunachaudhary186@gmail.com>
2023-01-09 23:07:33 +0530

    updated search bar

--- app.js
+++ app.js
@@ -90,6 +90,18 @@ const songs = [
     <div class="subtitle">Atif Aslam</div>`,
     poster: "img/15.jpg",
   },
+  {
+    id: "16",
+    songName: `Baarish <br>
+    <div class="subtitle">Ash King</div>`,
+    poster: "img/16.jpg",
+  },
+  {
+    id: "17",
+    songName: `Pehli Nazar Mein <br>
+    <div class="subtitle">Atif Aslam</div>`,
+    poster: "img/17.jpg",
+  },
 ];
 Array.from(document.getElementsByClassName("song-items")).forEach(
   (element, i) => {
@@ -97,6 +109,42 @@ Array.from(document.getElementsByClassName("song-items")).forEach(
     element.getElementsByTagName("h5")[0].innerHTML = songs[i].songName;
   }
 );
+
+// search data start
+let search_result = document.getElementsByClassName("search_result")[0];
+songs.forEach((element) => {
+  const { id, songName, poster } = element;
+  let card = document.createElement("a");
+  card.classList.add("card");
+  card.href = "#" + id;
+  card.innerHTML = `
+  <img src="${poster}" alt="">
+  <div class="content">
+      ${songName}         
+  </div>
+  `;
+  search_result.appendChild(card);
+});
+let input = document.getElementsByTagName("input")[0];
+input.addEventListener("keyup", () => {
+  let input_value = input.value.toUpperCase();
+  let items = search_result.getElementsByTagName("a");
+  for (let i = 0; i < items.length; i++) {
+    let as = items[i].getElementsByClassName("content")[0];
+    let text_value = as.textContent || as.innerHTML;
+    if (text_value.toUpperCase().indexOf(input_value) > -1) {
+      items[i].style.display = "flex";
+    } else {
+      items[i].style.display = "none";
+    }
+    if (input_value == 0) {
+      search_result.style.display = "none";
+    } else {
+      search_result.style.display = "";
+    }
+  }
+});
+// search data end
 
 let masterPlay = document.getElementById("masterPlay");
 let wave = document.getElementsByClassName("wave")[0];
